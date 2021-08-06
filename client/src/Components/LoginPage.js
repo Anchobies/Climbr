0930b8811925533005ae1b7901981553d4da2485
Anchobies <anthonychung97@gmail.com>
2021-08-06 04:29:31 -0400

    styled login page

--- client/src/Components/LoginPage.js
+++ client/src/Components/LoginPage.js
@@ -1,5 +1,27 @@
 import React, { useState } from 'react'
 import { Button } from "@material-ui/core"
+import HiveFive1 from '../HiveFive1.png'
+import HiveFive2 from '../HiveFive2.png'
+import HiveFive3 from '../HiveFive3.png'
+
+const style = {
+    background: '#ffda5b',
+    borderRadius: 3,
+    border: "0",
+    color: 'black',
+    height: 48,
+    padding: '0 30px',
+};
+
+const style2 = {
+    background: 'linear-gradient(45deg, #FE3B8B 10%, #Ff6E53 90%)',
+    borderRadius: 3,
+    border: 0,
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+};
 
 const LoginPage = ({ setToggle, toggle, onLogin }) => {
     const initialUser = {
@@ -38,27 +60,33 @@ const LoginPage = ({ setToggle, toggle, onLogin }) => {
     };
 
     return (
-        <div>
-            <header>Log In</header>
-            <form onSubmit={handleLogin} >
-                <br />
-                <label>
-                    <input onChange={handleCred} name="username" value={cred.username} type="text" placeholder="Username" />
-                </label>
-                <br />
-                <label>
-                    <input onChange={handleCred} name="password" value={cred.password} type="password" placeholder="Password" />
-                </label>
-                <br />
-                <br />
-                <button>Log In</button>
-                <br />
-                <br />
-            </form>
-            {loginErrors.map(loginError => <p className="error-message" key={loginError}>{loginError}</p>)}
-            <Button onClick={() => setToggle(!toggle)} type="submit" color="primary" variant="contained">
-                Register{" "}
-            </Button>
+        <div className="loginPage">
+            <img src={HiveFive3} className="loginLogo" />
+            <p className="loginDescription">A safe place to anonymously <br /> build each other up.</p>
+            <div className="loginForm">
+                <form onSubmit={handleLogin} >
+                    <br />
+                    <label>
+                        <input onChange={handleCred} name="username" value={cred.username} type="text" className="loginInput" placeholder="Username" />
+                    </label>
+                    <br />
+                    <br />
+                    <label>
+                        <input onChange={handleCred} name="password" value={cred.password} type="password" className="loginInput" placeholder="Password" />
+                    </label>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <Button style={style} type="submit" color="secondary" variant="contained" className="loginButton1">Log In</Button>
+                    <br />
+                </form>
+                {loginErrors.map(loginError => <p className="error-message" key={loginError}>{loginError}</p>)}
+                <Button style={style2} onClick={() => setToggle(!toggle)} type="submit" color="primary" variant="contained" className="loginButton2">
+                    Create New Account{" "}
+                </Button>
+            </div>
         </div>
     )
 }
